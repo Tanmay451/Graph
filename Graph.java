757264ff3529d306ccd9a42b0f9f6f163bea93cb
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2021-11-15 22:33:54 +0530

    detect cycle using bfs and dfs, finding is a graph bipartite or not using bfs and dfs

--- Graph.java
+++ Graph.java
@@ -1,5 +1,11 @@
 import java.util.*;
-
+class Node{
+    int current, parent;
+    public Node(int current, int parent){
+        this.current = current;
+        this.parent = parent;
+    }
+} 
 public class Graph {
     static ArrayList<Integer> bfs(int V,ArrayList<ArrayList<Integer>> adj){
         ArrayList<Integer> bfs = new ArrayList<Integer>();
@@ -49,27 +55,149 @@ public class Graph {
         adj.get(u).add(v);
         adj.get(v).add(u);
     }
+
+    static boolean isCycleHelper(int i, boolean vis[], ArrayList<ArrayList<Integer>> adg){
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(i,-1));
+        while(!q.isEmpty()){
+            int current = q.peek().current;
+            int parent = q.peek().parent;
+            q.remove();
+            for (Integer it : adg.get(current)){
+                if (!vis[it]){
+                    q.add(new Node(it,current));
+                    vis[it] = true;
+                } else if (it != parent){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    static boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
+        boolean vis[] = new boolean[V+1];
+        for (int i = 1; i<=V;i++){
+            if (!vis[i]){
+                if (isCycleHelper(i,vis, adj)) return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isCycleDFSHelper(int i, int parent, boolean vis[], ArrayList<ArrayList<Integer>> adj){
+        vis[i] = true;
+        for (Integer it : adj.get(i)){
+            if (!vis[it]){
+                if(isCycleDFSHelper(it, i, vis, adj)){
+                    return true;
+                }
+            }else if (it != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isCycleDFS(int V, ArrayList<ArrayList<Integer>> adj){
+        boolean vis[] = new boolean[V+1];
+        for(int i = 1;i<=V;i++){
+            if (!vis[i]){
+                if (isCycleDFSHelper(i,-1,vis,adj)) return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean isBipartiteHelper(int i, int vis[], ArrayList<ArrayList<Integer>> adj){
+        vis[i] = 0;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            for (Integer it : adj.get(node)){
+                if (vis[it] == -1){
+                    q.add(it);
+                    vis[it] = 1-vis[node];
+                }else if (vis[it] == vis[node]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static boolean isBipartite(int V, ArrayList<ArrayList<Integer>> adj){
+        int vis[] = new int[V+1];
+        for (int i = 0; i<=V;i++){
+            vis[i] = -1;
+        }
+        for (int i = 1; i<= V;i++){
+            if (vis[i] == -1){
+                if (!isBipartiteHelper(i, vis, adj)) return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isBipartiteDFSHelper(int i, int vis[], ArrayList<ArrayList<Integer>> adj, int c){
+        vis[i] = 1-c;
+        for (Integer it: adj.get(i)){
+            if (vis[it] == -1){
+                if (!isBipartiteDFSHelper(it, vis, adj, vis[i])) return false;
+            } else if (vis[it] == vis[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isBipartiteDFS(int V, ArrayList<ArrayList<Integer>> adj){
+        int vis[] = new int[V+1];
+        for (int i = 1; i<=V; i++){
+            vis[i] = -1;
+        }
+
+        for (int i = 1; i<=V;i++){
+            if (vis[i] == -1){
+                if (!isBipartiteDFSHelper(i, vis, adj, 1)) return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
         for (int i = 0;i<=7;i++)adj.add(new ArrayList<Integer>());
 
         addEdge(adj,1,2);
-        addEdge(adj,2,1);
         addEdge(adj,2,3);
         addEdge(adj,2,7);
-        addEdge(adj,3,2);
         addEdge(adj,3,5);
         addEdge(adj,4,6);
-        addEdge(adj,5,3);
         addEdge(adj,5,7);
-        addEdge(adj,6,4);
-        addEdge(adj,7,2);
-        addEdge(adj,2,5);
-        
+        // addEdge(adj,3,7);
+
+
+        System.out.println(adj);
         ArrayList<Integer> bfs = bfs(7,adj);
         System.out.println("bfs for given graph is :\t"+bfs);
 
         ArrayList<Integer> dfs = dfs(7,adj);
         System.out.println("dfs for given graph is :\t"+dfs);
+
+        boolean isCycle = isCycle(7, adj);
+        System.out.println("Circle in given graph :\t"+isCycle);
+
+        boolean isCycleDFS = isCycleDFS(7, adj);
+        System.out.println("Circle using DFS in given graph :\t"+isCycleDFS);
+
+        boolean isBipartite = isBipartite(7, adj);
+        System.out.println("isBipartite using BFS in given graph :\t"+isBipartite);
+
+        boolean isBipartiteDFS = isBipartiteDFS(7, adj);
+        System.out.println("isBipartiteDFS using BFS in given graph :\t"+isBipartiteDFS);
+
+
     }    
 }
